7b273dac00395e6da7619d821d5648a4ccf096ba
phillihoch <64873861+phillihoch@users.noreply.github.com>
2022-09-16 10:25:12 +0200

    add firebase

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components";
+import * as firebase from "firebase/app";
 
 import {
   useFonts as useOswald,
@@ -14,6 +15,17 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 import { Navigation } from "./src/infrastructure/navigation/index";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnA_3pl7nOr0HU6qxkN6btmrxQUBWXUag",
+  authDomain: "mealstogo-56027.firebaseapp.com",
+  projectId: "mealstogo-56027",
+  storageBucket: "mealstogo-56027.appspot.com",
+  messagingSenderId: "359810091408",
+  appId: "1:359810091408:web:d42a3f1e697a0d0ce60b15",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   let [oswaldLoaded] = useOswald({
